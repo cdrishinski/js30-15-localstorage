c1451acd7c1b6e390ce490281e3c2fd8728317d7
cdrishinski <cdrishinski@gmail.com>
2019-10-22 09:17:27 -0600

    logic added for checking off items, and suing local storage to maintain items

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e){
     // console.log("hey")
@@ -10,7 +10,30 @@ function addItem(e){
         text,
         done: false 
     }
+    items.push(item)
+    populateList(items, itemsList)
+    localStorage.setItem('items', JSON.stringify(items))
     this.reset();
 }
 
+function populateList(plates = [], platesList ) {
+    platesList.innerHTML = plates.map((plate, i) => {
+        return `
+            <li>
+                <input type="checkbox" data-index=${i} id="item${i}" ${plate.done?'checked': ''}/>
+                <label for="item${i}">${plate.text}</label>
+            </li>
+        `
+    }).join('')
+}
+
+function toggleDone(e){
+    if(!e.target.matches('input')) return; //skip if doesn't match
+    const el = e.target;
+    console.log(el.dataset.index)
+}
+
 addItems.addEventListener('submit', addItem)
+itemsList.addEventListener('click', toggleDone)
+
+populateList(items, itemsList)
